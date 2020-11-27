f5091eac9825e559eb812481dbc6398548558d8f
Ruebn <rubenmoreno.saladillo@gmail.com>
2020-11-27 13:23:03 +0100

    Screen 1 -> Create data class User implements Parcelable and @Parcelize

--- app/src/main/java/es/iessaladillo/pedrojoya/pr06/data/model/User.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr06/data/model/User.kt
@@ -1,9 +1,11 @@
 package es.iessaladillo.pedrojoya.pr06.data.model
 
-// TODO:
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 //  Crear una clase de datos User que implemente Parcelable y que
 //  contenga el id de tipo Long, nombre, email, phoneNumber, address, web y photoUrl
 //  todas ellas de tipo String.
-class User(){
-
-}
+@Parcelize
+data class User(val id: Long, val name: String, val email: String, val address: String, val web: String, val photoUrl: String): Parcelable
