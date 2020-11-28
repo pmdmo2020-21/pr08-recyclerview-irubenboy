0991d98815826324d69e6d1bc6943721d21ef3f7
Ruebn <rubenmoreno.saladillo@gmail.com>
2020-11-28 20:36:59 +0100

    Screen 2 -> Configure ViewModel and add random photo

--- app/src/main/java/es/iessaladillo/pedrojoya/pr06/ui/add_user/AddUserActivity.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr06/ui/add_user/AddUserActivity.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import es.iessaladillo.pedrojoya.pr06.R
 import es.iessaladillo.pedrojoya.pr06.databinding.UserActivityBinding
 
@@ -34,11 +36,18 @@ class AddUserActivity : AppCompatActivity() {
     // FIN NO TOCAR
 
     private val userBinding: UserActivityBinding by lazy { UserActivityBinding.inflate(layoutInflater) }
+    private val userViewModel: AddUserViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(userBinding.root)
+        setupViews()
     }
+
+    private fun setupViews() {
+        Glide.with(this).load(userViewModel.getRandomPhotoUrl()).into(userBinding.imgAvatar)
+    }
+
     private fun onSave() {
         // TODO: Acciones a realizar al querer salvar
     }
--- app/src/main/java/es/iessaladillo/pedrojoya/pr06/ui/add_user/AddUserViewModel.kt
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr06/ui/add_user/AddUserViewModel.kt
@@ -1,16 +1,21 @@
 package es.iessaladillo.pedrojoya.pr06.ui.add_user
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import es.iessaladillo.pedrojoya.pr06.data.model.User
+import java.util.*
 
-// TODO:
+
 //  Crear la clase EditUserViewModel. Ten en cuenta que la url de la photo
 //  deberá ser preservada por si la actividad es destruida por falta de recursos.
 
-class AddUserViewModel {
+class AddUserViewModel: ViewModel() {
+    private val random = Random()
 
     // Para obtener un URL de foto de forma aleatoria (tendrás que definir
     // e inicializar el random a nivel de clase.
-//    private fun getRandomPhotoUrl(): String =
-//            "https://picsum.photos/id/${random.nextInt(100)}/400/300"
+    fun getRandomPhotoUrl(): String =
+            "https://picsum.photos/id/${random.nextInt(100)}/400/300"
 
 }
